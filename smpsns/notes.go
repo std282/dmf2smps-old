3c395a19b6f8fc9ae0f199657a5b26663b9bd954
std282 <28.02.98.shohin@gmail.com>
2018-12-29 23:03:54 +0300

    added SMPS note calculation

---
+++ smpsns/notes.go
@@ -0,0 +1,41 @@
+package smpsns
+
+import "log"
+
+type note byte
+
+const (
+	// NoteC = C note
+	NoteC note = iota + 0x81
+	// NoteCs = C# note
+	NoteCs
+	// NoteD = D note
+	NoteD
+	// NoteDs = D# note
+	NoteDs
+	// NoteE = E note
+	NoteE
+	// NoteF = F note
+	NoteF
+	// NoteFs = F# note
+	NoteFs
+	// NoteG = G note
+	NoteG
+	// NoteGs = G# note
+	NoteGs
+	// NoteA = A note
+	NoteA
+	// NoteAs = A# note
+	NoteAs
+	// NoteB = B note
+	NoteB
+)
+
+// GetNote returns byte of a note
+func GetNote(nt note, oct int) byte {
+	if oct > 7 || (nt == NoteB && oct == 7) {
+		log.Fatal("smpsns: error: attempted to create SMPS note B-7 or higher")
+	}
+
+	return byte(nt) + byte(12*oct)
+}
