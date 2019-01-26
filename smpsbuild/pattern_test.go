50c4fd3097e4b1341560b058ad49c3f2c57ac900
std282 <28.02.98.shohin@gmail.com>
2019-01-26 21:59:07 +0300

    removed Pattern.PlaceNoteClever (responsibility tranferred to mngpat)

--- smpsbuild/pattern_test.go
+++ smpsbuild/pattern_test.go
@@ -3,7 +3,6 @@ package smpsbuild
 import (
 	"fmt"
 	"strings"
-	"testing"
 )
 
 type BinaryStringWriter struct {
@@ -31,35 +30,35 @@ func (bsw *BinaryStringWriter) Release() string {
 	return bsw.builder.String()
 }
 
-func TestPattern(t *testing.T) {
-	bsw := new(BinaryStringWriter)
-	bsw.Write([]byte{
-		0x00, 0x00,
-		0x06, 0x03,
-		0x01, 0x00,
-	})
+// func TestPattern(t *testing.T) {
+// 	bsw := new(BinaryStringWriter)
+// 	bsw.Write([]byte{
+// 		0x00, 0x00,
+// 		0x06, 0x03,
+// 		0x01, 0x00,
+// 	})
 
-	pat := NewPattern()
-	pat.PlaceNoteClever(0x81, 16)
-	pat.PlaceNoteClever(0x82, 16)
-	pat.PlaceNoteClever(0x81, 8)
-	pat.PlaceNoteClever(0x81, 8)
-	pat.PlaceNoteClever(0x82, 16)
-	pat.PlaceLoop(2, 5)
+// 	pat := NewPattern()
+// 	pat.PlaceNoteClever(0x81, 16)
+// 	pat.PlaceNoteClever(0x82, 16)
+// 	pat.PlaceNoteClever(0x81, 8)
+// 	pat.PlaceNoteClever(0x81, 8)
+// 	pat.PlaceNoteClever(0x82, 16)
+// 	pat.PlaceLoop(2, 5)
 
-	pat2 := NewPattern()
-	pat2.PlaceNoteClever(0x85, 16)
-	pat2.PlaceNoteClever(0x82, 16)
-	pat2.PlaceNoteClever(0x81, 16)
-	pat2.PlaceNoteClever(0x82, 16)
-	pat2.PlaceJump(pat)
+// 	pat2 := NewPattern()
+// 	pat2.PlaceNoteClever(0x85, 16)
+// 	pat2.PlaceNoteClever(0x82, 16)
+// 	pat2.PlaceNoteClever(0x81, 16)
+// 	pat2.PlaceNoteClever(0x82, 16)
+// 	pat2.PlaceJump(pat)
 
-	pat.updateRef(6)
-	pat2.updateRef(6 + pat.eventsSize)
+// 	pat.updateRef(6)
+// 	pat2.updateRef(6 + pat.eventsSize)
 
-	pat.represent(bsw)
-	pat2.represent(bsw)
-	bsw.Stop()
+// 	pat.represent(bsw)
+// 	pat2.represent(bsw)
+// 	bsw.Stop()
 
-	fmt.Print(bsw.Release())
-}
+// 	fmt.Print(bsw.Release())
+// }
