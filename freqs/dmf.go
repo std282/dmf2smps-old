4605ff61c1bfd88454fa693f64ccf18cce988475
std282 <28.02.98.shohin@gmail.com>
2019-01-27 01:20:13 +0300

    added DMF to SMPS note conversion

---
+++ freqs/dmf.go
@@ -0,0 +1,54 @@
+package freqs
+
+// DMF2SMPSNoteFM returns SMPS equivalent of DMF note for FM.
+func DMF2SMPSNoteFM(dmfNote, dmfOctave int16) byte {
+	switch dmfNote {
+	case -1:
+		logError.Fatal(
+			"empty notes are not supported for conversion",
+		)
+
+	case 100:
+		return 0x80
+	}
+
+	octaveShift := 12 * int(dmfOctave)
+	noteShift := int(dmfNote % 12)
+	note := byte(0x80 + noteShift + octaveShift)
+	switch note {
+	case 0xE0, 0xE1:
+		logWarn.Print(
+			"notes B-7/C-8 are not supported for FM conversion; will treat them as rest notes",
+		)
+
+		return 0x80
+	}
+
+	return byte(noteShift + octaveShift)
+}
+
+// DMF2SMPSNoteDispPSG returns SMPS equivalent of DMF note for PSG.
+func DMF2SMPSNoteDispPSG(dmfNote, dmfOctave int16) (note byte, disp int8) {
+	// Separately declare and initialize to allow recursion
+	var getFreq func(int) int
+	getFreq = func(n int) int {
+		freq := smpsPSGtableS3[n]
+
+		switch freq {
+		case -1:
+			return getFreq(n-12) / 2
+
+		case 0x3FF:
+			return 0x3F8 // lowest A
+
+		default:
+			return freq
+		}
+	}
+
+	pos := (dmfNote % 12) + (dmfOctave % 12)
+	freq := getFreq(int(pos))
+	note, disp = approxPeriodPSG(freq)
+	return
+
+}
