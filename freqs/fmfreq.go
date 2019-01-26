93a41f33f44dd993e07fa213006d276147cb6072
std282 <28.02.98.shohin@gmail.com>
2019-01-26 19:47:21 +0300

    fixed bugs with frequency displacement

--- freqs/fmfreq.go
+++ freqs/fmfreq.go
@@ -19,7 +19,7 @@ var smpsFMtable = [12]int{
 }
 
 // getEquFreqFM returns equivalent matched FM frequency
-func getEquFreqFM(note byte) int {
+func getEquFreqFM(note byte, disp int) int {
 	var octave uint
 	note -= 0x81
 	for note >= 12 {
@@ -27,11 +27,10 @@ func getEquFreqFM(note byte) int {
 		octave++
 	}
 
-	return smpsFMtable[note] << octave
+	return (smpsFMtable[note] + disp) << octave
 }
 
 func approxFreqFM(freq int) (note byte, disp int8) {
-
 	// Marginal frequency - maximum frequency that is representable in one FM
 	// octave range.
 	marginFreq := func() int {
@@ -66,6 +65,7 @@ func approxFreqFM(freq int) (note byte, disp int8) {
 			set,
 		)
 	}
+
 	switch {
 	case minDisp < -128:
 		warnAboutOverflow(minDisp, -128)
@@ -84,7 +84,7 @@ func approxFreqFM(freq int) (note byte, disp int8) {
 // OptimizeFM finds optimal match for note and its displacement. That is, it
 // finds such note that the displacement is as small as possible.
 func OptimizeFM(inNote byte, inDisp int) (outNote byte, outDisp int8) {
-	freq := getEquFreqFM(inNote) + inDisp
+	freq := getEquFreqFM(inNote, inDisp)
 	outNote, outDisp = approxFreqFM(freq)
 	return
 }
@@ -92,7 +92,7 @@ func OptimizeFM(inNote byte, inDisp int) (outNote byte, outDisp int8) {
 // DisplaceFM displaces optimized note to specified amount of semitones
 func DisplaceFM(inNote byte, inDisp int8, semitones float64) (outNote byte, outDisp int8) {
 	freq := func() int {
-		orig := getEquFreqFM(inNote) + int(inDisp)
+		orig := getEquFreqFM(inNote, int(inDisp))
 		mult := math.Pow(2, semitones/12)
 		resFreq := float64(orig) * mult
 		return int(resFreq)
@@ -105,7 +105,7 @@ func DisplaceFM(inNote byte, inDisp int8, semitones float64) (outNote byte, outD
 // DisplaceRawFM displaces unoptimized note to specified amount of semitones
 func DisplaceRawFM(inNote byte, inDisp int, semitones float64) (outNote byte, outDisp int8) {
 	freq := func() int {
-		orig := getEquFreqFM(inNote) + inDisp
+		orig := getEquFreqFM(inNote, inDisp)
 		mult := math.Pow(2, semitones/12)
 		resFreq := float64(orig) * mult
 		return int(resFreq)
